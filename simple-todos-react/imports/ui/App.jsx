53a945c7a8392b9a0e30fa7028bf67cc6abd278b
nardo88 <nardo1988@mail.ru>
2021-07-19 14:13:11 +0300

    events

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import {Task} from './Task';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
+import {TaskForm} from './TaskForm'
 
 
 export const App = () => {
 
 
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
   
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm />
+
       <ul>
    
         {
